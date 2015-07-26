105947554dac48df46b262ca005d2fdc45af7768
Luca Parolini <lucapar@gmail.com>
2015-07-26 16:39:49 +0200

    - Minor changes

--- plot3.R
+++ plot3.R
@@ -8,11 +8,17 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 NEI$yearFactor <- as.factor(NEI$year)
 NEI$SourceFactor <- as.factor(NEI$SCC)
+
+# 24510 is Baltimore
 idxToUse <- NEI$fips == 24510
+
+# Focus on the data of Baltimore
 Baltimore <- data.frame(NEI$Emissions[idxToUse], 
                         NEI$type[idxToUse],
                         as.factor(NEI$year[idxToUse]))
 names(Baltimore) <- c("Emissions", "Type", "Year")
+
+# Prepare now a data frame where to store total emissions per year and type
 nYears <- length(levels(Baltimore$Year))
 nTypes <- length(levels(Baltimore$Type))
 Year <- rep(3, nTypes * nYears)
@@ -31,6 +37,7 @@ for (iYear in levels(Baltimore$Year)) {
 }
 BaltimoreSummary <- data.frame(Emissions, Type, Year)
 
+# Plot
 png("plot3.png", width=480, height=480)
 myPlot <-qplot(Year, Emissions, data = BaltimoreSummary, color = Type)
 myPlot <- myPlot + labs(y="Total emissions (Tons)")
